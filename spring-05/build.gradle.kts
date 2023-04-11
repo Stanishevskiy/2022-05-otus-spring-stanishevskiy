d4f9d37879dcf75d9d73e9730a1150d1a4de046f
tolstjak <tolstjak100@gmail.com>
2023-04-11 13:36:13 +0400

    migrate project on Flyway DB migration system

--- spring-05/build.gradle.kts
+++ spring-05/build.gradle.kts
@@ -3,17 +3,25 @@ plugins {
     id("org.springframework.boot") version "3.0.5"
     id("io.spring.dependency-management") version "1.1.0"
     id("io.freefair.lombok") version "8.0.1"
+    id("org.flywaydb.flyway") version "9.16.3"
 }
 
 group = "ru.otus.spring"
 version = "1.0-SNAPSHOT"
 
+repositories {
+    mavenCentral()
+}
+
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
-repositories {
-    mavenCentral()
+flyway {
+    url = "jdbc:h2:mem:testdb"
+    user = "user"
+    password = "user"
+    locations = arrayOf("classpath:db/migration")
 }
 
 dependencies {
@@ -21,10 +29,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     implementation("org.springframework.shell:spring-shell-starter:3.0.1")
     implementation("com.h2database:h2")
+    implementation("org.flywaydb:flyway-core")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.assertj:assertj-core")
-
 }
 
 tasks.test {
---
+++ spring-05/src/test/resources/schema.sql
@@ -0,0 +1,23 @@
+drop table book if exists;
+drop table author if exists;
+drop table genre if exists;
+
+create table genre (
+    id int not null auto_increment,
+    name varchar(255) not null,
+    primary key (id)
+);
+create table author (
+    id int not null auto_increment,
+    name varchar(255) not null,
+    primary key (id)
+);
+create table book (
+    id int not null auto_increment,
+    name varchar(255) not null,
+    author_id int not null,
+    genre_id int not null,
+    primary key (id),
+    foreign key (author_id) references author(id),
+    foreign key (genre_id) references genre(id)
+);
